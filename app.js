baacdeadea105adacab380da86183f3b40abb449
user.name like : "Abhishek <user.email like : "adobriyal615@gmail.com>
2025-04-26 10:18:40 +0530

    feat: setup Routes - add /signup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express();
+const db = require('./db');
+const userRoutes = require('./routes/userRoutes');
 
 
 app.get('/', (req,res)=>{
---
+++ routes/userRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./../models/usermodel');
+
+router.get('/signup', async (req, res)=>{
+
+    try{
+        const data = req.body;
+
+        // fetching existing data
+        const existingEmail = User.findOne({email: data.email}) ;
+
+        // if email exits return
+        if(existingEmail){
+            return res.status(400).json({error: 'Email already exist'});
+        }
+
+        // creating new instance of model
+        const newUser = new User(data);
+
+        // saving data in db
+        const saved = await newUser.save();
+        console.log("data saved");
+        res.status(200).json(saved);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+
+});
+
+module.exports = router;
