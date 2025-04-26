f9674d6fcc0dd9ef9e4fc7df58d6ef3bcad66664
user.name like : "Abhishek <user.email like : "adobriyal615@gmail.com>
2025-04-26 07:25:29 +0530

    feat: add usermodel

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
 
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique : true,
+        required: true
+    },
+    email:{
+        type: String,
+        unique : true,
+        required : true
+    },
+    password: {
+        type : String,
+        required : true
+    },
+
+    data: [{
+        
+        content:{
+            type: String,
+            required : true,
+        }
+    }]
+
+});
