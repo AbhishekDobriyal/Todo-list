7ff4aae40b7a34ccd44268e85b91a9ffcc51e9e6
user.name like : "Abhishek <user.email like : "adobriyal615@gmail.com>
2025-04-27 16:03:51 +0530

    feat: add password encryption middleware

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -26,6 +26,29 @@ const userSchema = new mongoose.Schema({
     }]
 
 });
+
+userSchema.pre('save', async function(next){
+
+    // check if password is modified or not
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    try{
+        // creating salt
+        const salt = await bcrypt.genSalt(10);
+
+        // hashing (password) and adding salt to it and storing in db
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+
+    }
+    catch(err){
+        next(err);
+    }
+
+})
 
 const User = mongoose.model('User',userSchema);
 module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./../models/usermodel');
+const bcrypt = require('bcrypt');
 
 router.post('/signup', async (req, res)=>{
 
